b568482b279aeb86de217cb61a656b96141bdd33
Artur Gordashnikov <arturg@accessfintech.com>
2023-01-23 16:41:33 +0200

    Use file-scoped namespace

--- src/GraphQlClientGenerator/GraphQlGenerator.cs
+++ src/GraphQlClientGenerator/GraphQlGenerator.cs
@@ -154,13 +154,21 @@ using Newtonsoft.Json.Linq;
         writer.WriteLine();
         writer.WriteLine(RequiredNamespaces);
         writer.Write("namespace ");
-        writer.WriteLine(@namespace);
 
+        if (_configuration.CSharpVersion == CSharpVersion.Compatible)
+        {
+            writer.WriteLine(@namespace);
             writer.WriteLine("{");
-
-        Generate(new SingleFileGenerationContext(schema, writer, indentationSize: 4));
-
+            Generate(new SingleFileGenerationContext(schema, writer, indentationSize: 4));
             writer.WriteLine("}");
+        }
+        else
+        {
+            writer.Write(@namespace);
+            writer.WriteLine(";");
+            writer.WriteLine();
+            Generate(new SingleFileGenerationContext(schema, writer, indentationSize: 0));
+        }
 
         writer.Flush();
     }
